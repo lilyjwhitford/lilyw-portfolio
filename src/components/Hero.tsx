29cbe7d276cebf9cd5a67fb5ffd93ba7bda7956d
Lily Whitford <lilywhitford2@gmail.com>
2025-02-10 20:49:52 +0000

    attempt animanting hero section text lol

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const fadeUpVariants = {
+  hidden: { opacity: 0.01, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.4, ease: "easeOut" } },
+};
 
 const Hero: React.FC = () => {
   return (
-    <section className="min-h-screen flex flex-col items-start justify-center pl-48 -mt-11 ">
-      <h2 className="text-6xl font-semibold text-gray-700">Hi, I'm Lily :)</h2>
-      <h1 className="text-7xl font-bold text-gray-800 pt-2">
-        Full-Stack Web Developer
-      </h1>
-      <p className="text-xl text-gray-700 pt-2 text-left">
-        Passionate about designing seamless user experiences,
-      </p>
+    <section className="min-h-screen flex flex-col items-start justify-center pl-48 -mt-4 ">
+      <motion.div initial="hidden" animate="visible" variants={fadeUpVariants}>
+        <motion.h1 variants={fadeUpVariants} className="text-6xl font-semibold text-gray-700">Hi, I'm Lily :)</motion.h1>
+        <motion.h1 variants={fadeUpVariants} className="text-7xl font-bold text-gray-800 pt-2">Full-Stack Web Developer</motion.h1>
+        <motion.p variants={fadeUpVariants} className="text-xl text-gray-700 pt-2 text-left">Passionate about designing seamless user experiences,</motion.p>
+      </motion.div>
     </section>
   );
 };
