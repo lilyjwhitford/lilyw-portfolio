67b52d9ae932124899f066d5b1ad26b6444998a2
Lily Whitford <lilywhitford2@gmail.com>
2025-02-11 07:57:55 +0000

    make her look cute

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -27,21 +27,27 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
   const prevImage = () =>
     setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
 
+  // set container size based on app's main platform
   const containerStyles =
     platform === "mobile"
-      ? "w-[300px] h-[500px] max-w-[350px]"
+      ? "w-[150px] h-[250px] max-w-[250px]"
       : "w-[900px] h-[550px]";
 
+  // reverse the order of images for Lost&Confused
   const opposite = name === "Lost&Confused";
 
-  const extraSpacing = name === "Mrkt" ? "lg:gap-32" : "lg:gap-6";
+  // add extra spacing for Mrkt
+  const extraSpacing = name === "Mrkt" ? "lg:gap-20" : "lg:gap-16";
+
+  // keep text right-aligned for Mrkt and Photolabs
+  const isRightAligned = name === "Mrkt" || name === "PhotoLabs";
 
   return (
     <div
       className={`flex flex-col p-6 lg:items-center ${extraSpacing} ${
-        opposite ? "lg:flex-row-reverse" : "lg:flex-row"
-      }`}
-    >
+        opposite ? "lg:flex-row-reverse" : "lg:flex-row"}
+        ${isRightAligned ? "lg:text-right" : "lg:text-left"}
+    `}>
       <div
         className={`relative flex-1 flex items-center justify-center ${containerStyles}`}
       >
@@ -52,48 +58,48 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -50 }}
             transition={{ duration: 0.4, ease: "easeInOut" }}
-            className="absolute w-full h-full flex items-center justify-center"
+            className="absolute w-full h-full flex items-center justify-center z-10"
           >
-            <motion.img
-              src={images[currentIndex]}
-              alt={name}
-              className="w-auto h-auto object-contain rounded-xl"
-            />
+           <div className="p-2 border border-gray-300 shadow-md rounded-xl">
+      <motion.img
+        src={images[currentIndex]}
+        alt={name}
+        className="w-auto h-auto object-contain rounded-xl"
+      />
+    </div>
           </motion.div>
         </AnimatePresence>
 
         <button
-          className="absolute top-1/2 left-[-3rem] -translate-y-1/2 bg-gray-800 text-white rounded-full p-1 z-10 hover:bg-gray-700 transition"
+          className="absolute top-1/2 left-[-3rem] -translate-y-1/2 text-white bg-[#ecbfb1] rounded-full p-1 z-10 hover:bg-[#e2b0a1] transition"
           onClick={prevImage}
         >
           <ChevronLeft size={20} />
         </button>
         <button
-          className="absolute top-1/2 right-[-3rem] -translate-y-1/2 bg-gray-800 text-white rounded-full p-1 z-10 hover:bg-gray-700 transition"
+          className="absolute top-1/2 right-[-3rem] -translate-y-1/2 text-white bg-[#ecbfb1] rounded-full p-1 z-10 hover:bg-[#e2b0a1] transition"
           onClick={nextImage}
         >
           <ChevronRight size={20} />
         </button>
       </div>
 
-      <div
-        className={`flex-1 flex flex-col gap-4 ${
-          opposite ? "items-start" : "items-end"
-        }`}
-      >
-        <h3 className="text-2xl font-bold">{name}</h3>
-        <p className="text-gray-600">{description}</p>
-        <p className="text-gray-800 font-medium">
-          <span className="font-bold">Tech Stack:</span> {techStack}
-        </p>
-        <a
-          href={github}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center gap-2 text-gray-700 hover:text-black transition mt-4"
-        >
-        <FontAwesomeIcon icon={faGithub} className="text-3xl"/>
-        </a>
+      <div className={`flex-1`}>
+        <div className="bg-gradient-to-br from-[#f9e9df] to-[#ecbfb1] p-6 rounded-xl inline-block">
+          <div className={`flex flex-col gap-4 ${opposite ? "items-start" : "items-end"}`}>
+            <h3 className="text-gray-800 text-2xl font-bold">{name}</h3>
+            <p className="text-gray-600">{description}</p>
+            <p className="text-gray-800 font-medium">{techStack}</p>
+            <a
+              href={github}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2 text-gray-700 hover:text-black transition mt-4"
+            >
+              <FontAwesomeIcon icon={faGithub} className="text-3xl" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 import { projects } from "../helpers/data.ts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
 
 const Projects: React.FC = () => {
   return (
     <div className="min-h-screen px-16 pt-32 pb-32">
-      <h2 className="text-4xl text-gray-600 font-bold text-center mb-8">some of my work</h2>
+      <div className="w-full flex items-end justify-end mb-8">
+        <span className="block w-60 h-px bg-gray-400 mr-3 mb-8"></span>
+        <h2 className="text-5xl text-gray-600 font-bold relative top-4 pb-8">
+          some of my work
+          <FontAwesomeIcon icon={faStar} className="text-2xl px-3 pb-1" />
+        </h2>
+      </div>
+
       <div className="flex flex-col items-center justify-center gap-10">
         {projects.map((project) => (
           <ProjectItem
