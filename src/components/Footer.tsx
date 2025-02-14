b6d2b94cff7c69f6f924c40f05bb359bf76cd88b
Lily Whitford <lilywhitford2@gmail.com>
2025-02-14 21:14:09 +0000

    fix spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
       <>
-      <footer className="text-gray-500 text-sm text-center">
+      <footer className="text-gray-500 text-sm text-center -mt-[32px]">
         &copy; Lily Whitford | 2025
       </footer>
       </>
