b12675d31d1a03c6c5d4e21f0003e70ae4d3adfd
Lily Whitford <lilywhitford2@gmail.com>
2025-02-14 21:13:55 +0000

    add cute little gif and fix spacing

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,10 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
+import wavingHello from "../assets/wavingHello.gif";
+
 const Contact: React.FC = () => {
   return (
-    <section className="min-h-screen px-6 lg:px-10 py-16 flex flex-col items-center justify-center">
-      <h3 className="text-3xl font-bold text-gray-800 mb-4">come say hi!</h3>
+    <section className="min-h-screen px-6 lg:px-10 py-16 -mt-[48px] flex flex-col items-center justify-center">
+      <img src={wavingHello} alt="pusheen waving hand" className="w-32 h-32 mb-8" />
+      <div className="flex items-center w-full max-w-xl">
+        <div className="flex-grow h-px bg-gray-400"></div>
+        <h3 className="px-4 text-3xl font-bold text-gray-800">come say hi!</h3>
+        <div className="flex-grow h-px bg-gray-400"></div>
+      </div>
       <p className="text-center text-gray-600 max-w-2xl mb-8">
         Thanks for stopping by! If you have any questions or just want to chat, feel free to reach out to me on any of the platforms below. :)
       </p>
@@ -15,7 +22,7 @@ const Contact: React.FC = () => {
           href="https://github.com/lilyjwhitford"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-gray-600 hover:text-indigo-600 transition"
+          className="text-gray-600 hover:text-[#ecbfb1] transition"
         >
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
@@ -23,13 +30,13 @@ const Contact: React.FC = () => {
           href="https://www.linkedin.com/in/lily-whitford/"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-gray-600 hover:text-indigo-600 transition"
+          className="text-gray-600 hover:text-[#ecbfb1] transition"
         >
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
         <a
           href="mailto:lilywhitford2@gmail.com"
-          className="text-gray-600 hover:text-indigo-600 transition"
+          className="text-gray-600 hover:text-[#ecbfb1] transition"
         >
           <FontAwesomeIcon icon={faEnvelope} size="2x" />
         </a>
