72e4f6b3cba488eb4603c1f13f38161d3e8d6932
Lily Whitford <lilywhitford2@gmail.com>
2025-03-05 21:19:57 +0000

    redo the whoel damn thing

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,11 +3,13 @@ import { motion } from "framer-motion";
 import itMe from "../assets/itMe.jpeg";
 import { logos } from "../helpers/data";
 
+// fade up animation
 const fadeUpVariants = {
   hidden: { opacity: 0, y: 20 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
 };
 
+// stagger children for a nice effect
 const containerVariants = {
   hidden: {},
   visible: {
@@ -17,9 +19,19 @@ const containerVariants = {
   }
 };
 
+// hover animations
+const blockVariants = {
+  rest: { x: 0, y: 0 },
+  hover: { x: 5, y: 5 }
+};
+const imageVariants = {
+  rest: { x: 0, y: 0, filter: "grayscale(100%)" },
+  hover: { x: -6, y: -6, filter: "grayscale(0%)" },
+};
+
 const About: React.FC = () => {
   return (
-    <section className="min-h-screen px-6 lg:px-10 py-16 flex flex-col items-center justify-center">
+    <section className="min-h-screen px-6 lg:px-10 py-24 flex flex-col items-center justify-center">
       {/* about content */}
       <motion.div
         initial="hidden"
@@ -27,25 +39,38 @@ const About: React.FC = () => {
         viewport={{ once: true, amount: 0.5 }}
         variants={containerVariants}
       >
-
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center mb-16">
-          <div className="flex justify-center py-8">
-            <motion.img
-              src={itMe}
-              alt="picture of me with toad lol"
-              className="w-1/2 h-auto object-contain rounded-3xl shadow-lg filter grayscale hover:grayscale-0 transition duration-500"
-              transition={{ duration: 0.4 }}
-            />
+          {/* img column */}
+          <div className="flex justify-center">
+            <motion.div 
+              className="relative inline-block"
+              whileHover="hover"
+              initial="rest"
+              animate="rest"
+              transition={{ duration: 0.3, ease: "easeOut" }}
+            >
+              <motion.div 
+                className="absolute top-3 left-3 right-[-10px] bottom-[-10px] bg-[#ecbfb1] rounded-3xl z-[-1]"
+                variants={blockVariants}
+                transition={{ duration: 0.3, ease: "easeOut" }}
+              ></motion.div>
+              <motion.img
+                src={itMe}
+                alt="picture of me with toad lol"
+                className="w-[18rem] h-auto object-contain rounded-3xl shadow-lg"
+                variants={imageVariants}
+                transition={{ duration: 0.3, ease: "easeOut" }}
+              />
+            </motion.div>
           </div>
-
-          <div className="text-gray-600 max-w-lg flex items-end flex-col gap-6">
+          {/* text column */}
+          <div className="text-gray-600 max-w-lg flex items-start flex-col gap-6">
             <motion.div variants={fadeUpVariants}>
-              <div className="flex items-center">
+              <div className="flex items-center pl-12">
                 <span className="block w-60 h-px mt-10 bg-gray-400 mr-3"></span>
                 <h2 className="text-5xl font-bold pb-8 pt-16">about me</h2>
               </div>
             </motion.div>
-
             <motion.div variants={fadeUpVariants}>
               <div className="flex flex-col gap-2 text-right">
                 <p>
@@ -57,17 +82,17 @@ const About: React.FC = () => {
                 </p>
                 <p className="mt-4">
                   What started as a way to express myself online quickly turned into a drive to build
-                  and create in a meaningful way, transforming my internet addiction into an awesome career. Since then, I've been sharpening my skills in development and
-                  embracing new technologies with each project. When I'm not coding, you can find me
-                  exploring different restaurants and breweries, hitting the gym, or reading!
+                  and create in a meaningful way, transforming my internet addiction into an awesome career.
+                  Since then, I've been sharpening my skills in development and embracing new technologies with each project.
+                  When I'm not coding, you can find me exploring different restaurants and breweries, hitting the gym, or reading!
                 </p>
               </div>
             </motion.div>
-            </div>
           </div>
+        </div>
       </motion.div>
 
-      {/* technologies section */}
+      {/* tech section */}
       <motion.div
         initial="hidden"
         whileInView="visible"
